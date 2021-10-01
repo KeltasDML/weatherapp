7b4aff7955d2bf0ddf8b01ad0e0b3d4794008d0d
Vladyslav Yavorskyi <keltas2011@gmail.com>
2021-10-01 20:57:39 +0000

    More functionality

--- weatherapp.py
+++ weatherapp.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,abort
+from flask_sqlalchemy import SQLAlchemy
 # import json to load json data to python dictionary
 import json
 # urllib.request to make a request to api
@@ -6,16 +7,31 @@ import urllib.request
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_adress = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+
+
 def tofahrenheit(temp):
     return str(round((float(temp) - 273.16) * 9/5 + 32))
 
+@app.route('/weather',methods=['POST','GET'])
 @app.route('/',methods=['POST','GET'])
 def weather():
     api_key = 'e0e42510dbbc3629acc6497a7071fb13'
     if request.method == 'POST':
-        zipcode = request.form['zipcode']
+        rawZipcode = request.form['zipcode']
+        if rawZipcode.isdecimal() and len(rawZipcode) == 5:
+            zipcode = rawZipcode
+        else:
+            zipcode = '19107'
     else:
-        #defoult zipcode
+        #default zipcode
         zipcode = '19107'
 
     # source contain json data from api
@@ -41,6 +57,9 @@ def weather():
     }
     return render_template('index.html',data=data)
 
+@app.route('/settings')
+def settings_page():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
