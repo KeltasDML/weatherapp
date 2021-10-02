ca3c35dbb709cc268afa9f7b454c61d212a7a84c
Vladyslav Yavorskyi <keltas2011@gmail.com>
2021-10-02 01:04:33 +0000

    restructuring project

--- weatherapp.py
+++ weatherapp.py
@@ -1,15 +1,16 @@
-from flask import Flask,render_template,request,abort
+from flask import Flask,render_template,request,abort,flash
 from flask_sqlalchemy import SQLAlchemy
+import flask_login
 # import json to load json data to python dictionary
 import json
 # urllib.request to make a request to api
 import urllib.request
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
+#creating database to store User info
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -24,6 +25,7 @@ def tofahrenheit(temp):
 @app.route('/',methods=['POST','GET'])
 def weather():
     api_key = 'e0e42510dbbc3629acc6497a7071fb13'
+    
     if request.method == 'POST':
         rawZipcode = request.form['zipcode']
         if rawZipcode.isdecimal() and len(rawZipcode) == 5:
@@ -63,3 +65,27 @@ def settings_page():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
